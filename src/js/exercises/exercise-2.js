db7215e34bc5c59af71be053476f423877f5e6f5
raulnp89 <133123709+raulnp89@users.noreply.github.com>
2023-10-24 11:29:40 +0200

    Ejercicios 2 y 3

--- src/js/exercises/exercise-2.js
+++ src/js/exercises/exercise-2.js
@@ -3,3 +3,12 @@ EJERCICIO 2:
 Muestra el siguiente mensaje dependiendo de si un número es par o impar:
 “Number 34 is even” ó “Number 13 is odd”
 */
+
+
+let number = 34;
+
+if (number % 2 === 0) {
+  showContent(2, `Number ${number} is even`);
+} else {
+  showContent(2, `Number ${number} is odd`);
+}
--- src/js/exercises/exercise-3.js
+++ src/js/exercises/exercise-3.js
@@ -3,3 +3,18 @@ EJERCICIO 3:
 Muestra el siguiente mensaje con el resultado de contar el número de vocales que tiene un string:
 “Number of vowels is <result>”
 */
+
+
+const text = "Muestra el siguiente mensaje con el resultado de contar el número de vocales que tiene un string";
+
+let vowels = "aeiouáéíóú";
+let vowelCount = 0;
+
+for (let i = 0; i < text.length; i++) {
+  const letra = text.charAt(i);
+  if (vowels.includes(letra.toLowerCase())) {
+    vowelCount++;
+  }
+}
+
+showContent(3, `Number of vowels is ${vowelCount}`);
