e390794a4c80da760d6927da97e3e2f1a974b26c
raulnp89 <133123709+raulnp89@users.noreply.github.com>
2023-10-24 18:47:52 +0200

    Ejercicios 6 y 7

--- src/js/exercises/exercise-6.js
+++ src/js/exercises/exercise-6.js
@@ -3,3 +3,28 @@ EJERCICIO 6:
 Genera un array de 10 números aleatorios entre 0 y 100, calcula el menor y
 el mayor y muestra el resultado por pantalla
 */
+
+
+function generarNumAleatorio(min, max) {
+    return Math.round(Math.random() * 100);
+  }
+  let myArr = [];
+  for (i = 0; i < 10; i++) {
+    myArr.push(generarNumAleatorio(0, 100));
+  }
+  
+  let numeroMin = Math.min(...myArr);
+  let numeroMax = Math.max(...myArr);
+  
+  let total =
+    "El mínimo es " +
+    numeroMin +
+    "." +
+    "<br> El máximo es " +
+    numeroMax +
+    "." +
+    "<br>La lista de números aleatorios es: " +
+    myArr;
+  
+  showContent(6, total);
+  
--- src/js/exercises/exercise-7.js
+++ src/js/exercises/exercise-7.js
@@ -3,3 +3,18 @@ EJERCICIO 7:
 Escribir una función que calcule si una cadena de texto es un palíndromo y
 devuelva un mensaje con la cadena de texto y el resultado
 */
+
+
+function esPalindr(text) {
+    let textModif = text.toLowerCase();
+    textModif = textModif.replaceAll(" ", "");
+    let textReverse = textModif.split("").reverse().join("");
+    if (textModif === textReverse) {
+      return `${text} es un palíndromo.`;
+    } else {
+      return `${text} no es un palíndromo.`;
+    }
+  }
+  
+  showContent(7, esPalindr("Anita lava la tina"));
+
